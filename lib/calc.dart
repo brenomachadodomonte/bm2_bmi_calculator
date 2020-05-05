06f36c01b706d2c5dbaf31cf5c34fa5d323c3fe1
breno.monte <breno.monte@unimedteresina.com.br>
2020-05-05 13:42:27 -0300

    validating the form

--- lib/calc.dart
+++ lib/calc.dart
@@ -9,6 +9,7 @@ class _CalcState extends State<Calc> {
 
   final heightController = TextEditingController();
   final weightController  = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String _bmi  = '';
   String _result  = '';
 
@@ -21,56 +22,78 @@ class _CalcState extends State<Calc> {
       ),
       body: Container(
         padding: EdgeInsets.all(20),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text('BM2'),
-            Text('Body Mass Index', style: TextStyle(fontSize: 22)),
-            SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Your Height(cm)',
-                labelStyle: TextStyle(color: Colors.grey[700]),
-                border: OutlineInputBorder(),
-              ),
-              style: TextStyle(color: Colors.grey[700], fontSize: 25.0),
-              controller: heightController,
-              keyboardType: TextInputType.number,
-              //onChanged: onChanged,
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Your Weight(kg)',
-                labelStyle: TextStyle(color: Colors.grey[700]),
-                border: OutlineInputBorder(),
-              ),
-              style: TextStyle(color: Colors.grey[700], fontSize: 25.0),
-              controller: weightController,
-              keyboardType: TextInputType.number,
-              //onChanged: onChanged,
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                FloatingActionButton.extended(
-                  label: Text('Reset'),
-                  icon: Icon(Icons.clear),
-                  onPressed: _reset,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('BM2'),
+              Text('Body Mass Index', style: TextStyle(fontSize: 22)),
+              SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      validator: (text){
+                        if(text.isEmpty){
+                          return "Height is required!";
+                        } else if(int.parse(text) == 0){
+                          return "Invalid Height!";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Your Height(cm)',
+                        labelStyle: TextStyle(color: Colors.grey[700]),
+                        border: OutlineInputBorder(),
+                      ),
+                      style: TextStyle(color: Colors.grey[700], fontSize: 25.0),
+                      controller: heightController,
+                      keyboardType: TextInputType.number,
+                      //onChanged: onChanged,
+                    ),
+                    Padding(padding: EdgeInsets.all(10)),
+                    TextFormField(
+                      validator: (text){
+                        if(text.isEmpty){
+                          return "Weight is required!";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Your Weight(kg)',
+                        labelStyle: TextStyle(color: Colors.grey[700]),
+                        border: OutlineInputBorder(),
+                      ),
+                      style: TextStyle(color: Colors.grey[700], fontSize: 25.0),
+                      controller: weightController,
+                      keyboardType: TextInputType.number,
+                      //onChanged: onChanged,
+                    ),
+                  ],
                 ),
-                FloatingActionButton.extended(
-                  label: Text('Calc'),
-                  icon: Icon(Icons.check),
-                  onPressed: _calc,
-                ),
-              ],
-            ),
-            Padding(padding: EdgeInsets.all(20)),
-            Text(_bmi, style: TextStyle(fontSize: 18)),
-            Text(_result, style: TextStyle(fontSize: 26)),
-          ],
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  FloatingActionButton.extended(
+                    label: Text('Reset'),
+                    icon: Icon(Icons.clear),
+                    onPressed: _reset,
+                  ),
+                  FloatingActionButton.extended(
+                    label: Text('Calc'),
+                    icon: Icon(Icons.check),
+                    onPressed: _calc,
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(20)),
+              Text(_bmi, style: TextStyle(fontSize: 18)),
+              Text(_result, style: TextStyle(fontSize: 26)),
+            ],
+          ),
         ),
       ),
     );
@@ -86,25 +109,27 @@ class _CalcState extends State<Calc> {
   }
 
   void _calc(){
-    int height = int.parse(heightController.text);
-    int weight = int.parse(weightController.text);
+    if(_formKey.currentState.validate()){
+      int height = int.parse(heightController.text);
+      int weight = int.parse(weightController.text);
 
-    double bmi = weight / ((height / 100) * (height / 100));
-    String shape = '';
+      double bmi = weight / ((height / 100) * (height / 100));
+      String shape = '';
 
-    if(bmi < 18.5){
-      shape = 'underweight';
-    } else if(bmi < 25){
-      shape = 'normal weight';
-    } else if(bmi < 30){
-      shape = 'overweight';
-    } else {
-      shape = 'obesity';
+      if(bmi < 18.5){
+        shape = 'underweight';
+      } else if(bmi < 25){
+        shape = 'normal weight';
+      } else if(bmi < 30){
+        shape = 'overweight';
+      } else {
+        shape = 'obesity';
+      }
+
+      setState(() {
+        _bmi = 'Your BMI is ${bmi.toStringAsFixed(2)}';
+        _result = 'You are $shape';
+      });
     }
-
-    setState(() {
-      _bmi = 'Your BMI is ${bmi.toStringAsFixed(2)}';
-      _result = 'You are $shape';
-    });
   }
 }
